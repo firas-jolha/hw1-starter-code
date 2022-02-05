df461d367aea0c004a484131ee0820e40a4853e8
Firas Jolha <36399502+firas-jolha@users.noreply.github.com>
2022-02-05 18:26:14 +0300

    added run command

---
+++ src/main/java/iu/dsa/hw1/Main.java
@@ -0,0 +1,14 @@
+package iu.dsa.hw1;
+
+public class Main {
+
+	public static void main(final String[] args) {
+		System.out.println("Hello");
+		System.out.println("World ");
+		System.out.println("1");
+		System.out.println("4");
+		
+	}
+
+}
+
